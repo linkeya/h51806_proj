ebe2086feb349040b7a470137bd38f1ffed3c555
林可呀 <1761727379@qq.com>
2018-10-29 21:25:35 +0800

    2018-10-29

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const UserService = require("../services/user/user_service.js");
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-
-//登录:完整路径 “/api/users/login”
-router.post("/login",UserService.login);
-
-
-//注册：完整路径"/api/users/register"
-router.post("/register",UserService.register);
-
-//注销:完整路径 “/api/users/logout”
-router.post("/logout",UserService.logout);
 
 module.exports = router;
